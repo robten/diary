bcfd12520284e860b09d6c9884943b8c401c1083
Robten <robert.seemel@gmail.com>
2017-05-16 23:06:27 +0200

    configmanager: Beginning to extract functionality to a base class
    Signed-off-by: Robten <robert.seemel@gmail.com>

--- configmanager.py
+++ configmanager.py
@@ -4,22 +4,32 @@
 from configparser import ConfigParser
 
 
-class INImanager:
+class ManagerBase:
     def __init__(self, path=None, file=None):
         self._config_path = path
         self._config_file = file
-        self._config = ConfigParser()
         self.default_section = "DEFAULT"
+        self._data = {self.default_section: {}}
 
     def set(self, key, value, section=None):
         section = section if section else self.default_section
-        self._config.set(section, key, value)
+        self._data[section].__setitem__(key, value) # todo: handle implicit creation of a new section
 
     def get(self, key, section=None):
         section = section if section else self.default_section
-        return self._config.get(section, key)
+        return self._data[section].__getitem__(key) # todo: key not found exception
+
+    def get_data(self):
+        return self._data
+
+
+class INImanager(ManagerBase):
+    def __init__(self, *args, **kwargs):
+        super(INImanager, self).__init__(*args, **kwargs)
+        self._config = ConfigParser()
 
     def save(self):
+        self._config = self.get_data() # todo: search for alternativ method for assignment
         if self._config_path:
             with open(self._config_path, 'w') as file:
                 self._config.write(file)
