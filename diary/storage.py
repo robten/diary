008e355720d68d4370f841e593a84c51cfe77efb
Robten <robert.seemel@gmail.com>
2017-06-22 22:17:14 +0200

    storage: Adding methods to FileManager for setting attributes; and adding still empty methods for retrieving & deleting stored items (files).

--- diary/storage.py
+++ diary/storage.py
@@ -5,8 +5,8 @@ from os import path
 
 
 class FileManager:
-    def __init__(self, db_backend=None, compressed=False, root=None):
-        self._db = db_backend
+    def __init__(self, backend=None, compressed=False, root=None):
+        self._db = backend
         self._compression = compressed
         if root and path.exists(path.normpath(root)):
             self._root = path.normpath(root)
@@ -18,19 +18,28 @@ class FileManager:
             self._root = root
         else:
             raise NotADirectoryError()
+        
+    def set_backend(self, db):
+        self._db = db
+        
+    def set_compressed(self, state):
+        self._compression = state
 
     def ready(self):
         return self._root is not None
 
     def exists(self, item):
-        raise NotImplementedError()
+        pass
 
     def store(self, item,
               name=None, location=None, ftype=None, date=None, hierachy=None):
         pass
 
     def retrieve(self, item):
-        raise NotImplementedError()
+        pass
+
+    def delete(self, item):
+        pass
 
     def get_info(self, item):
-        raise NotImplementedError()
+        pass
