3371131ac74eaef5144a322b4af8e30f63aea593
Robten <robert.seemel@gmail.com>
2017-12-21 11:59:21 +0100

    database: Laying out the basic DbManager class.

--- diary/database.py
+++ diary/database.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
 
-class SQLiteManager:
-    pass
 
-# TODO: DB components to be implemented
+class DbManager:
+    def __init__(self, driver="sqlite", db=None, user=None, password=None, host=None,
+                 port=None):
+        self.engine = create_engine(URL(drivername=driver, database=db, host=host, port=port,
+                                        username=user, password=password))
+        self.Session = sessionmaker(bind=self.engine)
+        self.Model = declarative_base()
+        self._stage = None
+
+    def add(self, *args):
+        pass
+
+    def commit(self):
+        pass
+
+    def get(self):
+        pass
