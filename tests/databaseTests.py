172c1a2b555435e35fbfdd173bf540cd94228e82
Robten <robert.seemel@gmail.com>
2017-12-25 22:35:45 +0100

    database: Refactoring TestDbManager for extraction of duplicated initialization code.

--- tests/databaseTests.py
+++ tests/databaseTests.py
@@ -4,38 +4,35 @@
 import unittest
 from unittest.mock import MagicMock
 from diary.database import DbManager
+from diary.models import Entry
 
 
 class TestDbManager(unittest.TestCase):
+
+    def setUp(self):
+        self.db = DbManager()
+        self.db._session = MagicMock()
 
     def test_add(self):
         """
         Test if add() pushes the given parameters to the internaly used db session.
         """
-        manager = DbManager()
-        manager._session = MagicMock()
         test_entry1 = "not important"
         test_entry2 = "not important"
-        manager.add(test_entry1, test_entry2)
-        manager._session.add_all.assert_called_with(test_entry1, test_entry2)
+        self.db.add(test_entry1, test_entry2)
+        self.db._session.add_all.assert_called_with(test_entry1, test_entry2)
 
     def test_commit(self):
-        manager = DbManager()
-        manager._session = MagicMock()
-        manager.commit()
-        manager._session.commit.assert_called_with()
+        self.db.commit()
+        self.db._session.commit.assert_called_with()
 
     def test_get(self):
         """
         Test querying the database.
         """
-        class TestEntry:
-            pass
-        manager = DbManager()
-        manager._session = MagicMock()
-        manager._session.query.return_value(MagicMock())
-        manager.get(TestEntry).all()
-        manager._session.query.assert_called_with(TestEntry)
+        self.db._session.query.return_value(MagicMock())
+        self.db.get(Entry).all()
+        self.db._session.query.assert_called_with(Entry)
 
 
 if __name__ == '__main__':
