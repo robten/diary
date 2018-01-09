2fe6b7a988ed0c8a0cae071e12d89ae49a135beb
Robten <robert.seemel@gmail.com>
2018-01-09 18:55:34 +0100

    database: Bulky but working solution with one session as attribute of DbManager.

--- diary/database.py
+++ diary/database.py
@@ -12,14 +12,14 @@ class DbManager(Component):
     def __init__(self):
         super(DbManager, self).__init__()
         self.engine = self.invalid_state("engine", None)
-        self.Session = self.invalid_state("Session", None)
+        self.session = self.invalid_state("session", None)
         self._transaction = {"create": list(), "update": list(), "delete": list()}
 
     def initialize(self, driver="sqlite", db=None, user=None, password=None, host=None, port=None):
         self.engine = create_engine(URL(drivername=driver, database=db, host=host, port=port,
                                         username=user, password=password))
         Model.metadata.create_all(self.engine)
-        self.Session = sessionmaker(bind=self.engine)
+        self.session = sessionmaker(bind=self.engine)()
 
     def _transaction_add(self, section, *items):
         if section not in self._transaction.keys():
@@ -32,18 +32,36 @@ class DbManager(Component):
                 raise TypeError("Item of {} can't be appended to a commit.".format(type(item)))
 
     def create(self, *items):
-        self._transaction_add("create", items)
+        self._transaction_add("create", *items)
 
     @Component.dependent
     def read(self, *args, **kwargs):
         pass  # Implement read with own session
 
     def update(self, *items):
-        self._transaction_add("update", items)
+        self._transaction_add("update", *items)
 
     def delete(self, *items):
-        self._transaction_add("delete", items)
+        self._transaction_add("delete", *items)
 
     @Component.dependent
     def commit(self):
-        pass  # Implement committing _transaction in its own session
+        create_pending = len(self._transaction["create"]) > 0
+        update_pending = len(self._transaction["update"]) > 0
+        delete_pending = len(self._transaction["delete"]) > 0
+        if any([create_pending, update_pending, delete_pending]):
+            if create_pending:
+                for item in self._transaction["create"]:
+                    self.session.add(item)
+                self._transaction["create"].clear()
+            if update_pending:
+                for item in self._transaction["update"]:
+                    self.session.add(item)
+                self._transaction["update"].clear()
+            if delete_pending:
+                for item in self._transaction["delete"]:
+                    self.session.delete(item)
+                self._transaction["delete"].clear()
+            self.session.commit()
+        else:
+            return False
--- tests/databaseTests.py
+++ tests/databaseTests.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import unittest
+from unittest.mock import MagicMock
 from diary.database import DbManager
 from diary.models import Entry
 
@@ -43,6 +44,38 @@ class TestDbManager(unittest.TestCase):
         test2 = Entry(title="test2", text="test text2")
         with self.assertRaises(ValueError, msg="Invalid tag should raise ValueError."):
             self.db._transaction_add("wrong", test1, test2)
+
+    def test_commit_cleanup(self):
+        self.db.session = MagicMock()
+        test1 = Entry(title="test1", text="test text1")
+        test2 = Entry(title="test2", text="test text2")
+        test3 = Entry(title="test3", text="test text3")
+        self.db.create(test1)
+        self.db.update(test2)
+        self.db.delete(test3)
+        self.db.commit()
+        self.assertListEqual([], self.db._transaction["create"])
+        self.assertListEqual([], self.db._transaction["update"])
+        self.assertListEqual([], self.db._transaction["delete"])
+
+
+class TestDbManagerIntegration(unittest.TestCase):
+
+    def setUp(self):
+        self.db = DbManager()
+        self.db.initialize()
+
+    def test_commit(self):
+        test1 = Entry(title="test1", text="test text1")
+        test2 = Entry(title="test2", text="test text2")
+        self.db.create(test1, test2)
+        self.db.commit()
+        self.assertIsNotNone(test1.id)
+        self.assertIsNotNone(test2.id)
+        test1.title = "diverent title"
+        self.db.update(test1)
+        self.db.delete(test2)
+        self.db.commit()
 
 
 if __name__ == '__main__':
