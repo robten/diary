695464dbc59b5e2ae5f50128f4773f4de9ea5c77
Robten <robert.seemel@gmail.com>
2018-01-20 19:44:17 +0100

    models: Adding a SqlAlchemy model for files and establishing a many-to-many relationship. Fixing a value definition in Entry.

--- diary/models.py
+++ diary/models.py
@@ -2,15 +2,31 @@
 # coding: utf-8
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Date, Text
+from sqlalchemy.orm import relationship
 
 
 Model = declarative_base()
 
 
+entry_files = Table("entry_files", Model.metadata,
+                    Column("post_id", ForeignKey("entries.id"), primary_key=True),
+                    Column("file_id", ForeignKey("files.id"), primary_key=True))
+
+
 class Entry(Model):
-    __tablename__ = "entry"
+    __tablename__ = "entries"
     id = Column(Integer, primary_key=True)
-    title = Column(String(50))
-    text = Column(String)  # TODO Use SQL String for a large textfield?
+    title = Column(String(80))
+    text = Column(Text)
     timestamp = Column(Date)
+    files = relationship("File", secondary=entry_files, back_populates="entries")
+
+
+class File(Model):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    path = Column(String(120))
+    timestamp = Column(Date)
+    entries = relationship("Entry", secondary=entry_files, back_populates="files")
