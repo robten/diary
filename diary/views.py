625716b8afeecd444587db809cb02eeea2a937f7
Robten <robert.seemel@gmail.com>
2018-01-24 20:44:44 +0100

    models: Laying out SqlAlchemyQueryModel as custom QAbstractTableModel with methods to implement.

--- diary/views.py
+++ diary/views.py
@@ -2,7 +2,39 @@
 # coding: utf-8
 
 
-class TerminalView:
-    pass
+from PyQt5.QtCore import QAbstractTableModel, QModelIndex, Qt
 
-# TODO: View components to be implemented
+
+class SqlAlchemyQueryModel(QAbstractTableModel):
+    def __init__(self, parent=None):
+        super(SqlAlchemyQueryModel, self).__init__(parent)
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        pass
+
+    def data(self, index, role=Qt.DisplayRole):
+        pass
+
+    def setData(self, index, value, role=Qt.EditRole):
+        pass
+
+    def insertRow(self, row, parent=QModelIndex(), *args, **kwargs):
+        pass
+
+    def insertRows(self, row, count, parent=QModelIndex(), *args, **kwargs):
+        pass
+
+    def removeRow(self, row, parent=None, *args, **kwargs):
+        pass
+
+    def removeRows(self, row, count, parent=None, *args, **kwargs):
+        pass
+
+    def flags(self, index):
+        pass
+
+    def columnCount(self, parent=QModelIndex(), *args, **kwargs):
+        pass
+
+    def rowCount(self, parent=QModelIndex(), *args, **kwargs):
+        pass
