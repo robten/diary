e2d0a708cc0f28eb45140dd5507b843842bdf482
Robten <robert.seemel@gmail.com>
2018-04-14 14:14:15 +0200

    application: App: Replacing setup_database() and setup_storage() with config_init_db() and config_init_storage() to initialize these components with config data or defaults.

--- diary/application.py
+++ diary/application.py
@@ -16,6 +16,8 @@ class App(metaclass=MetaSingleton):
         self.db = db
         self.storage = storage
         self.view = view
+        self.default_driver = "sqlite"
+        self.default_root = "./"
 
     def load_config(self, path=None):
         if path:
@@ -25,6 +27,41 @@ class App(metaclass=MetaSingleton):
         else:
             raise ValueError("Config component isn't in a valid state, hence config can't load")
         #  TODO: check if any components can be set initially with loaded config file
+
+    def config_init_db(self, section="DATABASE"):
+        if not self.is_ready("config"):
+            return False
+        conf = self.config
+        if conf.has(section=section):
+            driver = conf.get("driver", section)\
+                if conf.has("driver", section) else self.default_driver
+            db = conf.get("db", section) if conf.has("db", section) else None
+            user = conf.get("user", section) if conf.has("user", section) else None
+            pw = conf.get("password", section) if conf.has("password", section) else None
+            host = conf.get("host", section) if conf.has("host", section) else None
+            port = conf.get("port", section) if conf.has("port", section) else None
+            self.db.initialize(driver=driver,
+                               db=db,
+                               user=user,
+                               password=pw,
+                               host=host,
+                               port=port)
+            return True
+        else:
+            return False
+
+    def config_init_storage(self, section="STORAGE"):
+        if not self.is_ready("storage"):
+            return False
+        conf = self.config
+        if conf.has(section=section):
+            location = conf.get("location", section)\
+                if conf.has("location", section) else self.default_root
+            compr = conf.get("compression", section) if conf.has("compression", section) else False
+            self.storage.initialize(root=location, compressed=compr)
+            return True
+        else:
+            return False
 
     def is_ready(self, component, raising=False):
         if not hasattr(self, component):
@@ -36,15 +73,6 @@ class App(metaclass=MetaSingleton):
                 raise TypeError("No valid component is set for '{}' in App.".format(component))
             return False
         return getattr(self, component).is_valid()
-
-    def setup_database(self, file=":memory:", user=None, password=None, url=None, db=None):
-        if user and password and url and db:
-            self.db.set(user=user, password=password, url=url, db=db)
-        else:
-            self.db.set(file=file)
-
-    def setup_storage(self, location=None):
-        self.storage.set(location=location)
 
     def setup_view(self, **kwargs):
         self.view.set(**kwargs)
